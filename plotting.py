f7539426c0df016c041a40ce65b33740c7fccf54
Jacob Bahnsen Schmidt <jacob.bahnsen.schmidt@gmail.com>
2021-12-03 09:51:24 +0100

    update plotting + fixing validation rewards

--- plotting.py
+++ plotting.py
@@ -29,7 +29,28 @@ plt.plot(x_val_rand_conv, total_training_reward_rand_conv, label='total training
 plt.plot(x_val_rand_conv,moving_average(total_training_reward_rand_conv), label = 'moving average rand conv')
 plt.xlabel('time steps'); plt.ylabel('reward')
 plt.xlim((0, max(x_val_baseline)*1.05))
-plt.legend(loc=4); plt.grid()
+plt.legend(loc=0); plt.grid()
+plt.tight_layout(); plt.show()
+
+
+total_validation_reward_baseline = torch.load('trainingResults/validation_Reward_baseline.pt')
+total_validation_reward_IMPALA = torch.load('trainingResults/validation_Reward_IMPALA.pt')
+total_validation_reward_rand_conv = torch.load('trainingResults/validation_Reward_IMPALA_rand_conv.pt')
+
+x_val_baseline = range(8192*2, (len(total_validation_reward_baseline)+1)*8192*2, 8192*2)
+x_val_IMPALA = range(8192*2, (len(total_validation_reward_IMPALA)+1)*8192*2, 8192*2)
+x_val_rand_conv = range(8192*2, (len(total_validation_reward_rand_conv)+1)*8192*2, 8192*2)
+
+plt.figure(figsize=(16,6))
+plt.plot(x_val_baseline, total_validation_reward_baseline, label='total validation reward baseline')
+plt.plot(x_val_baseline,moving_average(total_validation_reward_baseline), label = 'moving average baseline')
+plt.plot(x_val_IMPALA, total_validation_reward_IMPALA, label='total validation reward IMPALA')
+plt.plot(x_val_IMPALA,moving_average(total_validation_reward_IMPALA), label = 'moving average IMPALA')
+plt.plot(x_val_rand_conv, total_validation_reward_rand_conv, label='total validation reward rand conv')
+plt.plot(x_val_rand_conv,moving_average(total_validation_reward_rand_conv), label = 'moving average rand conv')
+plt.xlabel('time steps'); plt.ylabel('reward')
+plt.xlim((0, max(x_val_baseline)*1.05))
+plt.legend(loc=0); plt.grid()
 plt.tight_layout(); plt.show()
 
 
